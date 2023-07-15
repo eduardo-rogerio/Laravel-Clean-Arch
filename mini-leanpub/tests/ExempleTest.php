e2709935d26580eba9fd311f15354b6a5a7a3547
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-07-15 13:35:25 -0300

    aula 28

---
+++ mini-leanpub/tests/ExempleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\MiniLeanpub;
+
+use PHPUnit\Framework\TestCase;
+
+class ExempleTest extends TestCase
+{
+    public function testExemple()
+    {
+        $this->assertTrue(true);
+    }
+}
